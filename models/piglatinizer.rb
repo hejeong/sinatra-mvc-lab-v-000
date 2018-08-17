2ba08455494f518e14df08f9d11fad485227082a
Jonathan Hong <jonhong777@gmail.com>
2018-08-17 02:27:49 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
         consonant = letters.shift
         letters.push(consonant)
       end
-        letters.join('') + "ay"
+        letters.join('') + "way"
     end
   end
 
