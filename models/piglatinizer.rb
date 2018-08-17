3904d7a6f9343da7bf11befc96d832b15e15c088
Jonathan Hong <jonhong777@gmail.com>
2018-08-17 02:22:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,10 +2,12 @@ class PigLatinizer
 
   def piglatinize(word)
     # if begins with vowel
-    if word.match(/^[aeiouAEIOU].*/)
+    if word.match(/^[aeiouAEIOU].*/) != nil
       word + "ay"
+    else
       letters = word.split('')
       letters.
+    end
   end
 
 
